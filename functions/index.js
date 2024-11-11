a6e38630be02f7d291901ef6d843f4f9ca60be38
elahe199108@gmail.com <elahe199108@gmail.com>
2024-11-11 15:08:10 +0100

    added messaging notification

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+
+admin.initializeApp();
+
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+
+exports.sendEmailNotification = functions.firestore
+    .document("contactRequests/{docId}")
+    .onCreate((snap, context) => {
+        const data = snap.data();
+        const mailOptions = {
+            from: "your-email@gmail.com",
+            to: "your-email@gmail.com", // Email where you want to receive notifications
+            subject: `New Contact Request from ${data.name}`,
+            text: `Message from ${data.name} (${data.email}): ${data.message}`,
+        };
+
+        return transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error("Error sending email:", error);
+            } else {
+                console.log("Email sent:", info.response);
+            }
+        });
+    });
